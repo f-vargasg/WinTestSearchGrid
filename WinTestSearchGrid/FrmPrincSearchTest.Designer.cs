418e2b2f7bdecb9e01741d4327126901db862871
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-05-27 20:39:01 -0600

    Add project files.

---
+++ WinTestSearchGrid/FrmPrincSearchTest.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WinTestSearchGrid
+{
+    partial class FrmPrincTestSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgrData = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtToFind = new System.Windows.Forms.TextBox();
+            this.cmbSearchType = new System.Windows.Forms.ComboBox();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.butClearFilters = new System.Windows.Forms.Button();
+            this.txtFileToImport = new System.Windows.Forms.TextBox();
+            this.butImportFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRowCount = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.butOpenFile = new System.Windows.Forms.Button();
+            this.buTestDataset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrData)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgrData
+            // 
+            this.dgrData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgrData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrData.Location = new System.Drawing.Point(0, 65);
+            this.dgrData.Name = "dgrData";
+            this.dgrData.Size = new System.Drawing.Size(1202, 520);
+            this.dgrData.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serach by:";
+            // 
+            // txtToFind
+            // 
+            this.txtToFind.Location = new System.Drawing.Point(351, 18);
+            this.txtToFind.Name = "txtToFind";
+            this.txtToFind.Size = new System.Drawing.Size(268, 26);
+            this.txtToFind.TabIndex = 2;
+            // 
+            // cmbSearchType
+            // 
+            this.cmbSearchType.FormattingEnabled = true;
+            this.cmbSearchType.Location = new System.Drawing.Point(102, 18);
+            this.cmbSearchType.Name = "cmbSearchType";
+            this.cmbSearchType.Size = new System.Drawing.Size(206, 28);
+            this.cmbSearchType.TabIndex = 3;
+            // 
+            // butSearch
+            // 
+            this.butSearch.Location = new System.Drawing.Point(694, 21);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(96, 25);
+            this.butSearch.TabIndex = 4;
+            this.butSearch.Text = "Search";
+            this.butSearch.UseVisualStyleBackColor = true;
+            // 
+            // butClearFilters
+            // 
+            this.butClearFilters.Location = new System.Drawing.Point(815, 22);
+            this.butClearFilters.Name = "butClearFilters";
+            this.butClearFilters.Size = new System.Drawing.Size(96, 25);
+            this.butClearFilters.TabIndex = 5;
+            this.butClearFilters.Text = "Show All";
+            this.butClearFilters.UseVisualStyleBackColor = true;
+            // 
+            // txtFileToImport
+            // 
+            this.txtFileToImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtFileToImport.Location = new System.Drawing.Point(7, 20);
+            this.txtFileToImport.Name = "txtFileToImport";
+            this.txtFileToImport.Size = new System.Drawing.Size(369, 26);
+            this.txtFileToImport.TabIndex = 6;
+            // 
+            // butImportFile
+            // 
+            this.butImportFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.butImportFile.Location = new System.Drawing.Point(493, 20);
+            this.butImportFile.Name = "butImportFile";
+            this.butImportFile.Size = new System.Drawing.Size(234, 26);
+            this.butImportFile.TabIndex = 8;
+            this.butImportFile.Text = "Import Records File";
+            this.butImportFile.UseVisualStyleBackColor = true;
+            this.butImportFile.Click += new System.EventHandler(this.butImportFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(782, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Total No. of Records:";
+            // 
+            // lblRowCount
+            // 
+            this.lblRowCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblRowCount.AutoSize = true;
+            this.lblRowCount.Location = new System.Drawing.Point(976, 20);
+            this.lblRowCount.Name = "lblRowCount";
+            this.lblRowCount.Size = new System.Drawing.Size(21, 20);
+            this.lblRowCount.TabIndex = 10;
+            this.lblRowCount.Text = "...";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.butOpenFile);
+            this.panel1.Controls.Add(this.lblRowCount);
+            this.panel1.Controls.Add(this.txtFileToImport);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.butImportFile);
+            this.panel1.Location = new System.Drawing.Point(0, 591);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1202, 65);
+            this.panel1.TabIndex = 11;
+            // 
+            // butOpenFile
+            // 
+            this.butOpenFile.Location = new System.Drawing.Point(382, 19);
+            this.butOpenFile.Name = "butOpenFile";
+            this.butOpenFile.Size = new System.Drawing.Size(61, 28);
+            this.butOpenFile.TabIndex = 12;
+            this.butOpenFile.Text = "...";
+            this.butOpenFile.UseVisualStyleBackColor = true;
+            this.butOpenFile.Click += new System.EventHandler(this.butOpenFile_Click);
+            // 
+            // buTestDataset
+            // 
+            this.buTestDataset.Location = new System.Drawing.Point(980, 22);
+            this.buTestDataset.Name = "buTestDataset";
+            this.buTestDataset.Size = new System.Drawing.Size(168, 25);
+            this.buTestDataset.TabIndex = 12;
+            this.buTestDataset.Text = "Test Dataset";
+            this.buTestDataset.UseVisualStyleBackColor = true;
+            this.buTestDataset.Click += new System.EventHandler(this.buTestDataset_Click);
+            // 
+            // FrmPrincTestSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.buTestDataset);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.butClearFilters);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.cmbSearchType);
+            this.Controls.Add(this.txtToFind);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgrData);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmPrincTestSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dinamic Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dgrData)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgrData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtToFind;
+        private System.Windows.Forms.ComboBox cmbSearchType;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Button butClearFilters;
+        private System.Windows.Forms.TextBox txtFileToImport;
+        private System.Windows.Forms.Button butImportFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRowCount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button butOpenFile;
+        private System.Windows.Forms.Button buTestDataset;
+    }
+}
+
---
+++ WinTestSearchGrid/FrmPrincSearchTest.cs
@@ -0,0 +1,186 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTestSearchGrid
+{
+    public partial class FrmPrincTestSearch : Form
+    {
+        public FrmPrincTestSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void butOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Archivos de Excel (*.xls;*.xlsx)|*.xls;*.xlsx"; //le indicamos el tipo de filtro en este caso que busque
+            //solo los archivos excel
+            dialog.Title = "Seleccione el archivo de Excel";//le damos un titulo a la ventana
+            dialog.FileName = string.Empty;//inicializamos con vacio el nombre del archivo
+            //si al seleccionar el archivo damos Ok
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFileToImport.Text = dialog.FileName;
+            }
+        }
+
+        private void butImportFile_Click(object sender, EventArgs e)
+        {
+            // HSSFWorkbook hssfwb;
+            XSSFWorkbook hssfwb;
+            try
+            {
+                using (FileStream file = new FileStream(txtFileToImport.Text, FileMode.Open, FileAccess.Read))
+                {
+                    hssfwb = new XSSFWorkbook(file);
+                }
+
+                XSSFFormulaEvaluator eval = new XSSFFormulaEvaluator(hssfwb);
+
+                // int maxColumns = 2;
+
+                ISheet sheet = hssfwb.GetSheet("Sheet1");
+                for (int row = 0; row <= sheet.LastRowNum; row++)
+                {
+                    if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+                    {
+                        for (int ncol = 0; ncol < sheet.GetRow(row).LastCellNum; ncol++)
+                        {
+                            ICell icel = sheet.GetRow(row).GetCell(ncol);
+                            if (icel != null)
+                            {
+                                // string value = icel.StringCellValue;
+                                string value = string.Empty;
+                                switch (icel.CellType)
+                                {
+                                    case CellType.Unknown:
+                                        break;
+                                    case CellType.Numeric:
+                                        value = icel.NumericCellValue.ToString();
+                                        break;
+                                    case CellType.String:
+                                        value = icel.StringCellValue;
+                                        break;
+                                    case CellType.Formula:
+                                        value = ProcessFormula(icel, eval);
+                                        break;
+                                    case CellType.Blank:
+                                        value = string.Empty;
+                                        break;
+                                    case CellType.Boolean:
+                                        value = (icel.BooleanCellValue ? "true" : "false");
+                                        break;
+                                    case CellType.Error:
+                                        value = "Error";
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                string salida = string.Format("Row {0} = {1}", row, value);
+                                Console.WriteLine(salida);
+                                // MessageBox.Show(salida);
+                            }
+                        }
+
+                        //MessageBox.Show(string.Format("Row {0} = {1}", row, sheet.GetRow(row).GetCell(0).StringCellValue));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show (ex.Message);
+            }
+            
+        }
+
+        private static string ProcessFormula( ICell icel, 
+                                              XSSFFormulaEvaluator eval)
+        {
+            string value;
+            CellValue cv = eval.Evaluate(icel);
+            value = string.Empty;
+            switch (cv.CellType)
+            {
+                case CellType.Unknown:
+                    break;
+                case CellType.Numeric:
+                    value = cv.NumberValue.ToString();
+                    break;
+                case CellType.String:
+                    value = cv.StringValue;
+                    break;
+                case CellType.Formula:
+                    break;
+                case CellType.Blank:
+                    break;
+                case CellType.Boolean:
+                    break;
+                case CellType.Error:
+                    break;
+                default:
+                    break;
+            }
+
+            return value;
+        }
+
+        private void buTestDataset_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt;
+            DataRow dr;
+            DataColumn idCoulumn;
+            DataColumn nameCoulumn;
+            int i = 0;
+
+            dt = new DataTable();
+            idCoulumn = new DataColumn("ID", Type.GetType("System.Int32"));
+            nameCoulumn = new DataColumn("Name", Type.GetType("System.String"));
+
+            dt.Columns.Add(idCoulumn);
+            dt.Columns.Add(nameCoulumn);
+
+            dr = dt.NewRow();
+            dr["ID"] = 1;
+            dr["Name"] = "Name1";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["ID"] = 2;
+            dr["Name"] = "Name2";
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+
+            /*
+            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                MessageBox.Show(ds.Tables[0].Rows[i].ItemArray[0] + " -- " + ds.Tables[0].Rows[i].ItemArray[1]);
+            }
+            */
+
+            DataTable dtAll = ds.Tables[0].Copy();
+            for (i = 1; i < ds.Tables.Count; i++)
+            {
+                dtAll.Merge(ds.Tables[i]);
+            }
+            dgrData.AutoGenerateColumns = true;
+            dgrData.DataSource = dtAll;
+
+        }
+    }
+}
